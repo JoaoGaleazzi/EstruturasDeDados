1b5d5f5c9eaef13646235b49f31b25829ab6de17
Joao Galeazzi <joaohstolf@gmail.com>
2024-05-10 19:46:07 -0300

    trabalho pronto

---
+++ Estrutura de Dados/Trabalhos/ExpressaoAritmetica/Calculadora/src/Lista/ListaEncadeada.java
@@ -0,0 +1,108 @@
+package Lista;
+
+public class ListaEncadeada<T> {
+    private NoLista<T> primeiro;
+
+    public ListaEncadeada() {
+        super();
+    }
+
+    public NoLista<T> getPrimeiro(){
+        return primeiro;
+    }
+
+    public void inserir(T info){
+        NoLista<T> novoNo = new NoLista<T>();
+        novoNo.setInfo(info);
+        if(!estaVazia()){
+            novoNo.setProximo(primeiro);
+        }
+        primeiro = novoNo;
+    }
+
+    public void inserirNoFinal(T info){
+        NoLista<T> novoNo = new NoLista<T>();
+        novoNo.setInfo(info);
+        if(estaVazia()){
+            primeiro = novoNo;
+            return;
+        }
+        NoLista<T> noAtual = primeiro;
+        while(noAtual.getProximo() != null){
+            noAtual = noAtual.getProximo();
+        }
+        noAtual.setProximo(novoNo);
+    }
+
+    public boolean estaVazia(){
+        return primeiro == null;
+    }
+
+    public NoLista<T> buscar(T info){
+        NoLista<T> noAtual = primeiro;
+        while(noAtual != null){
+            if(noAtual.getInfo().equals(info))
+                break;
+            noAtual = noAtual.getProximo();
+        }
+        return noAtual;
+    }
+
+    public void retirar(T info){
+        if(!estaVazia() && primeiro.getInfo().equals(info)){
+            primeiro = primeiro.getProximo();
+            return;
+        }
+        NoLista<T> noAtual = primeiro;
+        while(noAtual.getProximo() != null){
+            if(noAtual.getProximo().getInfo().equals(info)){
+                noAtual.setProximo(noAtual.getProximo().getProximo());
+                return;
+            }
+            noAtual = noAtual.getProximo();
+        }
+    }
+
+    public int obterComprimento(){
+        int count = 0;
+        NoLista<T> noAtual = primeiro;
+        while(noAtual != null){
+            count++;
+            noAtual = noAtual.getProximo();
+        }
+        return count;
+    }
+
+    public NoLista<T> obterNo(int idx){
+        if(idx < 0 || estaVazia())
+            throw new IndexOutOfBoundsException();
+        
+        NoLista<T> noAtual = primeiro;
+        for (int i = 0; i < idx; i++) {
+            if(noAtual == null)
+                throw new IndexOutOfBoundsException();
+            noAtual = noAtual.getProximo();
+        }
+
+        return noAtual;
+    }
+
+    @Override
+	public String toString() {
+        if(estaVazia())
+            return "";
+        
+		String resultado = "";
+        NoLista<T> noAtual = primeiro;
+
+		resultado += noAtual.getInfo().toString();
+        while(noAtual != null){
+            if(!noAtual.equals(primeiro))
+                resultado += ", " + noAtual.getInfo();
+            noAtual = noAtual.getProximo();
+        }
+
+		
+		return resultado;
+	}
+}
--- Estrutura de Dados/Trabalhos/ExpressaoAritmetica/Calculadora/src/trabalho/Calculadora.java
+++ Estrutura de Dados/Trabalhos/ExpressaoAritmetica/Calculadora/src/trabalho/Calculadora.java
@@ -8,8 +8,7 @@ public class Calculadora {
         FilaLista<String> filaExpressao = extrairTermos(expressao);
         FilaLista<String> filaExpressaoPosFixada = gerarExprPosFixada(filaExpressao);
 
-
-        return 0;
+        return calcularExprPosFixada(filaExpressaoPosFixada);
     }
 
     private FilaLista<String> extrairTermos(String expressao) throws Exception{
@@ -80,14 +79,80 @@ public class Calculadora {
                 continue;
             }
 
-            
+            String termosEncerrar = "(";
 
+            switch (termo) {
+                case "*":
+                case "/":
+                    termosEncerrar += "+-";
+                    break;
+
+                case "+":
+                case "-":
+                    break;
+                
+                default:
+                    throw new Exception("Expressão Inválida");
+            }
+            
+            while(!pilha.estaVazia()){
+                String termoPilha = pilha.peek();
+                if(termosEncerrar.contains(termoPilha)){
+                    break;
+                }
+                novaFila.inserir(pilha.pop());
+            }
+            pilha.push(termo);
         }
 
-
-
-
+        while(!pilha.estaVazia()){
+            novaFila.inserir(pilha.pop());
+        }
 
         return novaFila;
+    }
+
+    private float calcularExprPosFixada(FilaLista<String> expPosFixada) throws Exception{
+        try {
+            PilhaLista<String> aux = new PilhaLista<String>();
+
+            while(!expPosFixada.estaVazia()){
+                String termo = expPosFixada.retirar();
+                if("+-*/".contains(termo)){
+                    aux.push(calcularTermo(aux.pop(), aux.pop(), termo));
+                }
+                else{
+                    aux.push(termo);
+                }
+            }
+
+            float resultado = Float.parseFloat(aux.pop());
+
+            if(!aux.estaVazia())
+                throw new Exception("Expressão inválida");
+
+            return resultado;
+        }
+         catch (Exception e) {
+            throw new Exception("Expressão inválida");
+        }
+        
+    }
+
+    private String calcularTermo(String st1, String st2, String operador) throws Exception{
+        float n1 = Float.parseFloat(st1);
+        float n2 = Float.parseFloat(st2);
+        switch (operador) {
+            case "*":
+                return Float.toString(n2 * n1);
+            case "/":
+                return Float.toString(n2 / n1);
+            case "+":
+                return Float.toString(n2 + n1);
+            case "-":
+                return Float.toString(n2 - n1);
+            default:
+                throw new Exception();
+        }
     }
 }
